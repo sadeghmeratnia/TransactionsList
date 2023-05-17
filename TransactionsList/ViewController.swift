d4f826cd42dc6aa18606c3c42b9b3b4bcbb4cd7d
Sadegh Meratnia <sadegh_meratnia@outlook.com>
2023-05-17 10:31:55 +0330

    Initial Commit

---
+++ TransactionsList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TransactionsList
+//
+//  Created by Sadegh on 5/17/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
